15cc270ce5bf40d5554efe1462752cdae4b17a20
Jockey780 <xlordhitmanx@gmail.com>
2024-03-17 17:28:48 +0700

    Assignment function(CreateAssignment, UpdateAsignment GetAssignmentsInCourse, GetAssignmentById)

---
+++ App_DataAccessObject/Mappers/AssignmentMapper.cs
@@ -0,0 +1,20 @@
+﻿using App_BusinessObject.DTOs.Request.Assignment;
+using App_BusinessObject.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_DataAccessObject.Mappers
+{
+    public class AssignmentMapper : Profile
+    {
+        public AssignmentMapper() 
+        {
+            CreateMap<CreateAssignmentRequest, Assignment>();
+            CreateMap<Assignment, UpdateAssignmentRequest>();
+        }
+    }
+}
