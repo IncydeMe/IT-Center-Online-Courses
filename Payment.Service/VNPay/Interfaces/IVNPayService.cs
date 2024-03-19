df3d9d76ea4d65e1ff73dad76028ef0fad0c8aa3
hntduong12345 <hntduong2003@gmail.com>
2024-03-19 15:43:37 +0700

    Add third party service
    Include Firebase for file storing and VNPay for payment, transaction

---
+++ Payment.Service/VNPay/Interfaces/IVNPayService.cs
@@ -0,0 +1,16 @@
+﻿using Payment.Domain.VNPay.Request;
+using Payment.Domain.VNPay.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Service.VNPay.Interfaces
+{
+    public interface IVNPayService
+    {
+        public Task<string> GetPaymentLink(string baseUrl, string secretKey, VNPayRequest request);
+        public Task<bool> IsValidSignature(string secretKey, VNPayResponse response);
+    }
+}
