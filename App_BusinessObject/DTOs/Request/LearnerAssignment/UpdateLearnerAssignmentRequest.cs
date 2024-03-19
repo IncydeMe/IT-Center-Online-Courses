397495a44a7716d62ae9d4923aee46ebcb3138fc
Jockey780 <xlordhitmanx@gmail.com>
2024-03-19 20:08:34 +0700

    Create and Update LearnerAssignment

---
+++ App_BusinessObject/DTOs/Request/LearnerAssignment/UpdateLearnerAssignmentRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_BusinessObject.DTOs.Request.LearnerAssignment
+{
+    public class UpdateLearnerAssignmentRequest
+    {
+        public int AccountId { get; set; }
+        public int AssignmentId { get; set; }
+        public float Mark { get; set; }
+        public DateTime AssignmentTakenDate { get; set; }
+        public DateTime TakenDuration { get; set; }
+    }
+}
