742b95498c20a7cda2e5d38f2a8219134ef805a9
NguyenTruongKhangThuan <ntkthuan1209@gmail.com>
2024-03-25 14:15:27 +0700

    Fix Errors

--- IT Center Website/Program.cs
+++ IT Center Website/Program.cs
@@ -1,7 +1,47 @@
+
+using App_Repository;
+using App_Repository.Interfaces;
+using App_Repository.Repositories;
+using App_Service;
+using App_Service.Interfaces;
+using App_Service.Services;
+using ITCenterService;
+using Payment.Service.VNPay.Interfaces;
+using Payment.Service.VNPay.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAssignmentRepository, AssignmentRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<ILessonRepository, LessonRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOwnedCourseRepository, OwnedCourseRepository>();
+builder.Services.AddScoped<IOwnedLessonRepository, OwnedLessonRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<ILearnerAssignmentRepository, LearnerAssignmentRepository>();
+
+
+
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IAssignmentService, AssignmentService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddScoped<IFeedbackService, FeedbackService>();
+builder.Services.AddScoped<ILessonService, LessonService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOwnedCourseService, OwnedCourseService>();
+builder.Services.AddScoped<IOwnedLessonService, OwnedLessonService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<ILearnerAssignmentService, LearnerAssignmentService>();
+builder.Services.AddScoped<IVNPayService, VNPayService>();
 
 var app = builder.Build();
 
