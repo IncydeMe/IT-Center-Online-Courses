65fd70a75b64c71db34dadfe3cb0c9db8d02f623
stonk-over-flown <fyeah0515@gmail.com>
2024-03-29 00:20:46 +0700

    edit account pages for admin (wip)

--- IT Center Website/Pages/Admin/Accounts/Create.cshtml.cs
+++ IT Center Website/Pages/Admin/Accounts/Create.cshtml.cs
@@ -1,3 +1,6 @@
+using App_BusinessObject.DTOs.Request.Account;
+using App_Service.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,28 @@ namespace IT_Center_Website.Pages.Admin.Accounts
 {
     public class CreateStaffAccountModel : PageModel
     {
-        public void OnGet()
+        private readonly IAccountService _accountService;
+        private readonly IMapper _mapper;
+
+        public CreateStaffAccountModel(IAccountService accountService, IMapper mapper)
         {
+            _accountService = accountService;
+            _mapper = mapper;
+        }
+
+        [BindProperty]
+        public CreateAccountRequest CreateAccountRequest { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _accountService.CreateAccount(CreateAccountRequest);
+
+            return RedirectToPage("Index");
         }
     }
 }
